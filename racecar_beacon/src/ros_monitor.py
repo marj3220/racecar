40ac6030c9478d37014da8d1d9ceff178a75fdec
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-09-09 14:47:27 -0400

    Start of ros_monitor
    Added callbacks

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import rospy
 import socket
 import threading
 
+import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
+from tf.transformations import euler_from_quaternion
+
+
+def quaternion_to_yaw(quat) -> float:
+    # Uses TF transforms to convert a quaternion to a rotation angle around Z.
+    # Usage with an Odometry message: 
+    #   yaw = quaternion_to_yaw(msg.pose.pose.orientation)
+    (roll, pitch, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+    return yaw
 
 class ROSMonitor:
     def __init__(self):
         # Add your subscriber here (odom? laserscan?):
-        # self.sub_odom = rospy.Subcriber(...)
-        # self.sub_laser = rospy.Subscriber(...)
+        self.sub_odom = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_cb)
+        self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.obstacle_cb)
 
         # Current robot state:
         self.id = 0xFFFF
@@ -32,6 +41,16 @@ class ROSMonitor:
         # self.rr_socket = socket.Socket(...)
         while True:
             pass
+
+    # Odometry Callback:
+    def odom_cb(self, msg: Odometry):
+        temp_pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, quaternion_to_yaw(msg.pose.pose.orientation))
+        self.pos = temp_pos
+
+    # Obstacble Callback: 
+    def obstacle_cb(self, msg: LaserScan):
+        temp_obstacle = not(all(range>1 for range in msg.ranges))
+        self.obstacle = temp_obstacle
 
 if __name__=="__main__":
     rospy.init_node("ros_monitor")
