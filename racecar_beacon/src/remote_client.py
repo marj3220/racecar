8865cfa5c7b7bcaabaae35a4cb39ce23bb7b83d2
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-09-09 15:50:24 -0400

    Finished ROS Message subscription and started RPC Server

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -6,3 +6,25 @@ HOST = '127.0.0.1'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65432
 
+#CEST PAS DU BON CODE
+self.rr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+self.rr_socket.bind((self.host, self.remote_request_port))
+
+    while True:
+        self.rr_socket.listen(1)
+        (conn, addr) = self.rr_socket.accept()
+        # RPC Loop
+        while True:
+            request = conn.recv(1024)
+            request = str(request, encoding="ascii")
+            print(request)
+            if not request: break
+            data = bytes("RPC requested is not valid")
+            if request == "RPOS":
+                data = bytes(self.pos)
+            elif request == "OBSF":
+                data = bytes(self.obstacle)
+            elif request == "RBID":
+                data = bytes(self.id)
+            conn.send(data)
+        conn.close()
--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -13,7 +13,7 @@ def quaternion_to_yaw(quat) -> float:
     # Uses TF transforms to convert a quaternion to a rotation angle around Z.
     # Usage with an Odometry message: 
     #   yaw = quaternion_to_yaw(msg.pose.pose.orientation)
-    (roll, pitch, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+    (_, _, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
     return yaw
 
 class ROSMonitor:
@@ -21,24 +21,54 @@ class ROSMonitor:
         # Add your subscriber here (odom? laserscan?):
         self.sub_odom = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_cb)
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.obstacle_cb)
-
+        
         # Current robot state:
         self.id = 0xFFFF
         self.pos = (0,0,0)
         self.obstacle = False
+
+        # Host:
+        self.host = '127.0.0.1'
 
         # Params :
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
 
         # Thread for RemoteRequest handling:
-        self.rr_thread = threading.Thread(target=self.rr_loop)
+        self.rr_thread = threading.Thread(target=self.remote_request_loop)
+        # Thread for PositionBroadcast handling
+        self.pb_thread = threading.Thread(target=self.position_broadcast_loop)
 
         print("ROSMonitor started.")
 
-    def rr_loop(self):
-        # Init your socket here :
-        # self.rr_socket = socket.Socket(...)
+    def remote_request_loop(self):
+        # Init of socket
+        self.rr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rr_socket.bind((self.host, self.remote_request_port))
+        while True:
+            self.rr_socket.listen(1)
+            (conn, addr) = self.rr_socket.accept()
+            # RPC Loop
+            while True:
+                request = conn.recv(1024)
+                if not request: break
+                request = str(request, encoding="ascii")
+                print(request)
+                data = bytes("RPC requested is not valid")
+                if request == "RPOS":
+                    data = bytes(self.pos)
+                elif request == "OBSF":
+                    data = bytes(self.obstacle)
+                elif request == "RBID":
+                    data = bytes(self.id)
+                conn.send(data)
+            conn.close()
+
+    def position_broadcast_loop(self):
+        # Init of socket
+        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.pb_socket.connect((self.host, self.pos_broadcast_port))
+        # RPC Loop
         while True:
             pass
 
@@ -58,5 +88,3 @@ if __name__=="__main__":
     node = ROSMonitor()
 
     rospy.spin()
-
-
