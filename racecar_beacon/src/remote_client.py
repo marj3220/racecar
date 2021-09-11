b583c560f25f709a35ec42047ef83da38638ec04
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-09-11 15:48:53 -0400

    Finished primary code that works on loopback IP

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from ros_monitor import Serializer
 import socket
+
+from ros_monitor import Serializer
 
 
 class remoteClient:
--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import socket
-from struct import *
 import threading
+from struct import *
 
 import rospy
 from nav_msgs.msg import Odometry
@@ -10,16 +10,20 @@ from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
 
 
-def quaternion_to_yaw(quat) -> float:
-    # Uses TF transforms to convert a quaternion to a rotation angle around Z.
+def quaternion_to_yaw(quat):
+    """ Uses TF transforms to convert a quaternion to a rotation angle around Z. """
     # Usage with an Odometry message: 
     # yaw = quaternion_to_yaw(msg.pose.pose.orientation)
     (_, _, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
     return yaw
 
+def ip2int(addr):
+    return unpack("!I", socket.inet_aton(addr))[0]
+
 class Serializer:
     RPOS_format = "fffxxxxx"
     OBSF_RBID_format = "Ixxxxxxxxxxxx"
+    PB_format = "fffI"
 
     @staticmethod
     def from_info_to_byte_RPOS(data):
@@ -30,7 +34,11 @@ class Serializer:
         return pack(Serializer.OBSF_RBID_format, data)
     
     @staticmethod
-    def from_byte_to_info(format: str, encoded_data):
+    def from_info_to_byte_PB(id, data):
+        return pack(Serializer.PB_format, data[0], data[1], data[2], id)
+    
+    @staticmethod
+    def from_byte_to_info(format, encoded_data):
         return unpack(format, encoded_data)
 
 class ROSMonitor:
@@ -39,23 +47,24 @@ class ROSMonitor:
         self.sub_odom = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_cb)
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.obstacle_cb)
         # Current robot state:
-        self.id = 0xFFFF
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        self.id = ip2int(local_ip)
         self.pos = (0,0,0)
         self.obstacle = False
         # Host:
-        self.host = '127.0.0.1'
+        self.host = '127.0.0.1' #change to local_ip when on RPi
         # Params :
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
         # Thread for RemoteRequest handling:
         self.rr_thread = threading.Thread(target=self.remote_request_loop)
         # Thread for PositionBroadcast handling
-        self.pb_thread = threading.Thread(target=self.position_broadcast_loop)
+        self.position_broadcast_init()
+        self.timer = rospy.Timer(rospy.Duration(1.0), self.position_broadcast)
         print("ROSMonitor started.")
         self.rr_thread.start()
-        self.pb_thread.start()
         self.rr_thread.join()
-        self.pb_thread.join()
     
     def remote_request_loop(self):
         # Init of socket
@@ -72,7 +81,6 @@ class ROSMonitor:
                 request = conn.recv(1024)
                 if not request: break
                 request = request.decode()
-                print(request)
                 data = str.encode("RPC requested is not valid")
                 if request == "RPOS":
                     data = Serializer.from_info_to_byte_RPOS(self.pos)
@@ -83,29 +91,28 @@ class ROSMonitor:
                 conn.send(data)
             conn.close()
 
-    def position_broadcast_loop(self):
-        # Init of socket
-        #self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.pb_socket.connect((self.host, self.pos_broadcast_port))
-        # RPC Loop
-        while True:
-            pass
+    def position_broadcast_init(self):
+        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.pb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.pb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.pb_socket.bind(('255.255.255.255', self.pos_broadcast_port))
+        self.pb_socket.settimeout(99999)
+        print("PositionBroadcast Service has started")
+    
+    def position_broadcast(self, event):
+        self.pb_socket.sendto(Serializer.from_info_to_byte_PB(self.id, self.pos), ('<broadcast>', self.pos_broadcast_port))
 
     # Odometry Callback:
-    def odom_cb(self, msg: Odometry):
+    def odom_cb(self, msg):
         temp_pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, quaternion_to_yaw(msg.pose.pose.orientation))
         self.pos = temp_pos
 
-    # Obstacble Callback: 
-    def obstacle_cb(self, msg: LaserScan):
+    # Obstacble Callback:
+    def obstacle_cb(self, msg):
         temp_obstacle = not(all(range>1 for range in msg.ranges))
         self.obstacle = temp_obstacle
 
-
-
 if __name__=="__main__":
     rospy.init_node("ros_monitor")
-
     node = ROSMonitor()
-
     rospy.spin()
