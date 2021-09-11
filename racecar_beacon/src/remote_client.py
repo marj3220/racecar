9812d57f6dadae5c9cd48aaa3a1e0cd31a227fc1
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-09-11 14:36:55 -0400

    Finished RemoteClient and corresponding ROS

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -1,30 +1,86 @@
 #!/usr/bin/env python
 
+from ros_monitor import Serializer
 import socket
 
-HOST = '127.0.0.1'
-# This process should listen to a different port than the PositionBroadcast client.
-PORT = 65432
 
-#CEST PAS DU BON CODE
-self.rr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-self.rr_socket.bind((self.host, self.remote_request_port))
+class remoteClient:
+    def __init__(self):
+        self.HOST = '127.0.0.1'
+        # This process should listen to a different port than the PositionBroadcast client.
+        self.PORT = 65432
+        self.socket_init()
 
-    while True:
-        self.rr_socket.listen(1)
-        (conn, addr) = self.rr_socket.accept()
-        # RPC Loop
+    def __del__(self):
+        try:
+            self.rc_socket.close()
+        except:
+            pass
+
+    def socket_init(self):
+        self.rc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rc_socket.connect((self.HOST, self.PORT))
+    
+    def showMainMenu(self):
         while True:
-            request = conn.recv(1024)
-            request = str(request, encoding="ascii")
-            print(request)
-            if not request: break
-            data = bytes("RPC requested is not valid")
-            if request == "RPOS":
-                data = bytes(self.pos)
-            elif request == "OBSF":
-                data = bytes(self.obstacle)
-            elif request == "RBID":
-                data = bytes(self.id)
-            conn.send(data)
-        conn.close()
+            print("\nDrop-down menu: ")
+            print("\t1. Set Connection Params (Already connected to default vehicule)")
+            print("\t2. Send Command to Server")
+            print("\t3. Close connection")
+            try:
+                menu_option = int(input("Select from drop-down: "))
+                self.executeMainMenu(menu_option)
+            except:
+                print("Menu option was not a number")
+
+    def executeMainMenu(self, main_menu_request: int):
+        if main_menu_request == 1:
+            self.setConnectionParam()
+        elif main_menu_request == 2:
+            self.menuCmd()
+        elif main_menu_request == 3:
+            self.rc_socket.close
+        else:
+            print("Invalid request")
+
+    def setConnectionParam(self):
+        self.rc_socket.close()
+        self.HOST = str(input("Enter HOST IP Address:"))
+        self.PORT = str(input("Enter Port number :"))
+        self.socket_init()
+
+    def menuCmd(self):
+        # RPC Loop
+        print("\t1. RPOS")
+        print("\t2. OBSF")
+        print("\t3. RBID")
+        RPC_Request = int(input("Input wanted RPC's number: "))
+        try:
+            self.sendCmd(RPC_Request)
+        except Exception as e:
+            print("Socket has timed out, retrying connection, error: ", e)
+            self.socket_init()
+    
+    def sendCmd(self, RPC_Request: int):
+        if RPC_Request == 1:
+            data = str.encode("RPOS")
+            format = Serializer.RPOS_format
+        elif RPC_Request == 2:
+            data = str.encode("OBSF")
+            format = Serializer.OBSF_RBID_format
+        elif RPC_Request == 3:
+            data = str.encode("RBID")
+            format = Serializer.OBSF_RBID_format
+        else:
+            print("Invalid request")
+            return
+        self.rc_socket.send(data)
+        data = self.rc_socket.recv(1024)
+        decoded_data = Serializer.from_byte_to_info(format, data)
+        print("\nReceived data:")
+        for data in decoded_data:
+            print("\t",str(data))
+
+if __name__=="__main__":
+    client = remoteClient()
+    client.showMainMenu()
--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+from struct import *
 import threading
 
 import rospy
@@ -12,62 +13,80 @@ from tf.transformations import euler_from_quaternion
 def quaternion_to_yaw(quat) -> float:
     # Uses TF transforms to convert a quaternion to a rotation angle around Z.
     # Usage with an Odometry message: 
-    #   yaw = quaternion_to_yaw(msg.pose.pose.orientation)
+    # yaw = quaternion_to_yaw(msg.pose.pose.orientation)
     (_, _, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
     return yaw
+
+class Serializer:
+    RPOS_format = "fffxxxxx"
+    OBSF_RBID_format = "Ixxxxxxxxxxxx"
+
+    @staticmethod
+    def from_info_to_byte_RPOS(data):
+        return pack(Serializer.RPOS_format, data[0], data[1], data[2])
+
+    @staticmethod
+    def from_info_to_byte_OBSF_RBID(data):
+        return pack(Serializer.OBSF_RBID_format, data)
+    
+    @staticmethod
+    def from_byte_to_info(format: str, encoded_data):
+        return unpack(format, encoded_data)
 
 class ROSMonitor:
     def __init__(self):
         # Add your subscriber here (odom? laserscan?):
         self.sub_odom = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_cb)
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.obstacle_cb)
-        
         # Current robot state:
         self.id = 0xFFFF
         self.pos = (0,0,0)
         self.obstacle = False
-
         # Host:
         self.host = '127.0.0.1'
-
         # Params :
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
-
         # Thread for RemoteRequest handling:
         self.rr_thread = threading.Thread(target=self.remote_request_loop)
         # Thread for PositionBroadcast handling
         self.pb_thread = threading.Thread(target=self.position_broadcast_loop)
-
         print("ROSMonitor started.")
-
+        self.rr_thread.start()
+        self.pb_thread.start()
+        self.rr_thread.join()
+        self.pb_thread.join()
+    
     def remote_request_loop(self):
         # Init of socket
         self.rr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.rr_socket.bind((self.host, self.remote_request_port))
+        self.rr_socket.settimeout(99999)
+        print("RemoteRequest Service has started")
         while True:
             self.rr_socket.listen(1)
-            (conn, addr) = self.rr_socket.accept()
+            (conn, _) = self.rr_socket.accept()
+            print("Connected to client")
             # RPC Loop
             while True:
                 request = conn.recv(1024)
                 if not request: break
-                request = str(request, encoding="ascii")
+                request = request.decode()
                 print(request)
-                data = bytes("RPC requested is not valid")
+                data = str.encode("RPC requested is not valid")
                 if request == "RPOS":
-                    data = bytes(self.pos)
+                    data = Serializer.from_info_to_byte_RPOS(self.pos)
                 elif request == "OBSF":
-                    data = bytes(self.obstacle)
+                    data = Serializer.from_info_to_byte_OBSF_RBID(self.obstacle)
                 elif request == "RBID":
-                    data = bytes(self.id)
+                    data = Serializer.from_info_to_byte_OBSF_RBID(self.id)
                 conn.send(data)
             conn.close()
 
     def position_broadcast_loop(self):
         # Init of socket
-        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.pb_socket.connect((self.host, self.pos_broadcast_port))
+        #self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.pb_socket.connect((self.host, self.pos_broadcast_port))
         # RPC Loop
         while True:
             pass
@@ -81,6 +100,8 @@ class ROSMonitor:
     def obstacle_cb(self, msg: LaserScan):
         temp_obstacle = not(all(range>1 for range in msg.ranges))
         self.obstacle = temp_obstacle
+
+
 
 if __name__=="__main__":
     rospy.init_node("ros_monitor")
